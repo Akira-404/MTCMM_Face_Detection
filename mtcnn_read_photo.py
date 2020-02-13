4f22516b6b125a758167a95353533ba5974a2e1e
omega-Lee <2033211717@qq.com>
2020-02-13 21:36:43 +0800

    修复路径问题

--- mtcnn_read_photo.py
+++ mtcnn_read_photo.py
@@ -10,8 +10,9 @@ dist = []
 name_tmp = []
 Emb_data = []
 image_tmp = []
-img = 'Face/nana.jpg'
+img = 'faceset/nana.jpg'
 img_color = 'red'
+
 
 def to_rgb(img):
     w, h = img.shape
@@ -32,11 +33,11 @@ while True:
     frame = cv2.imread(img)
     frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25, interpolation=cv2.INTER_NEAREST)
     # frame = cv2.resize(frame, (int(400), int(400)), interpolation=cv2.INTER_LINEAR)
-    cv2.imshow('src',frame)
+    cv2.imshow('src', frame)
     bounding_boxes, _ = align.detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
     faces_num = bounding_boxes.shape[0]  # 人脸数目
     print('bounding_boxes:', bounding_boxes)
-    print('_:',_)
+    print('_:', _)
     print('找到人脸数目为：{}'.format(faces_num))
     print(bounding_boxes.shape)
     print(bounding_boxes)
@@ -50,7 +51,8 @@ while True:
         cv2.rectangle(frame, (face_position[0], face_position[1]), (face_position[2], face_position[3]), (0, 255, 0), 2)
 
     cv2.imshow('demo', frame)
-    key = cv2.waitKey(3)#按下esc退出
+    key = cv2.waitKey(3)  # 按下esc退出
     if key == 27:
         break
+
 cv2.destroyAllWindows()
