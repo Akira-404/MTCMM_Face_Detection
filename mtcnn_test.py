5267fd2762b92f2a2f813f812d739136e4dd1fdb
omega-Lee <2033211717@qq.com>
2020-02-26 17:28:38 +0800

    删除一些无用的文件

--- mtcnn_test.py
+++ mtcnn_test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import cv2
 import align.detect_face
-import numpy as np
+import os
 
 minsize = 20  # 脸部最小尺寸
 threshold = [0.6, 0.7, 0.7]  # 三步阈值
@@ -11,12 +11,9 @@ name_tmp = []
 Emb_data = []
 image_tmp = []
 img_path = 'faceset/nana.jpg'
-img_color = 'red'
 
 
 def read_photo(img):
-    print(' 建立mtcnn人脸检测模型，加载参数')
-    gpu_memory_fraction = 1.0
     with tf.Graph().as_default():
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0, allow_growth=True)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
@@ -24,24 +21,20 @@ def read_photo(img):
             pnet, rnet, onet = align.detect_face.create_mtcnn(sess, 'align/')
 
     frame = cv2.imread(img)
-    frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25, interpolation=cv2.INTER_NEAREST)
+    frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
 
     cv2.imshow('src', frame)
-    bounding_boxes, _ = align.detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+    bounding_boxes, points = align.detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
     faces_num = bounding_boxes.shape[0]  # 人脸数目
-    print('bounding_boxes:', bounding_boxes)
-    print('_:', _)
+    print('bounding_boxes.shape:', bounding_boxes.shape, '\n bounding_boxes:', bounding_boxes)
+    print('points shape:', points.shape, '\n points:', points)
     print('找到人脸数目为：{}'.format(faces_num))
-    print(bounding_boxes.shape)
-    print(bounding_boxes)
-    print(np.squeeze(bounding_boxes))
-    print(np.squeeze(bounding_boxes).shape)
 
-    crop_faces = []
     for face_position in bounding_boxes:
         face_position = face_position.astype(int)
-        # print(face_position[0:4])
         cv2.rectangle(frame, (face_position[0], face_position[1]), (face_position[2], face_position[3]), (0, 255, 0), 1)
+        cv2.circle(frame, (face_position[0], face_position[1]), 2, (0, 0, 255), -1)
+        cv2.circle(frame, (face_position[2], face_position[3]), 2, (0, 0, 255), -1)
 
     writer = tf.summary.FileWriter('logs/', sess.graph)
     cv2.imshow('demo', frame)
