65dc560f0dfcf54a7b3573b85149465491c70ff9
omega-Lee <2033211717@qq.com>
2020-02-26 17:31:37 +0800

    删除无用的东西

--- align/detect_face.py
+++ align/detect_face.py
@@ -1,8 +1,4 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 from six import string_types, iteritems
-
 import numpy as np
 import tensorflow as tf
 import cv2
