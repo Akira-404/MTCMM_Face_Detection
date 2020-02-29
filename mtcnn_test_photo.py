1eae470f5779aab4cbef3a4e758f3e818f5f024e
omega-Lee <2033211717@qq.com>
2020-02-29 15:33:23 +0800

    部分优化

--- mtcnn_test_photo.py
+++ mtcnn_test_photo.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import cv2
 import align.detect_face
-import os
 import numpy as np
 
 minsize = 20  # 脸部最小尺寸
@@ -11,23 +10,34 @@ dist = []
 name_tmp = []
 Emb_data = []
 image_tmp = []
-img_path = 'faceset/img2.jpg'
+img_path = 'faceset/img.jpg'
 
 
 def read_photo(img):
     with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0, allow_growth=True)
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        gpu_options = tf.GPUOptions(
+            per_process_gpu_memory_fraction=1.0,
+            allow_growth=True)
+        sess = tf.Session(
+            config=tf.ConfigProto(
+                gpu_options=gpu_options,
+                log_device_placement=False))
         with sess.as_default():
             pnet, rnet, onet = align.detect_face.create_mtcnn(sess, 'align/')
 
     frame = cv2.imread(img)
-    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5,
+                       interpolation=cv2.INTER_AREA)
 
     cv2.imshow('src', frame)
-    bounding_boxes, points = align.detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+    bounding_boxes, points = align.detect_face.detect_face(
+        frame, minsize, pnet, rnet, onet, threshold, factor)
     faces_num = bounding_boxes.shape[0]  # 人脸数目
-    print('bounding_boxes.shape:', bounding_boxes.shape, '\n bounding_boxes:', bounding_boxes)
+    print(
+        'bounding_boxes.shape:',
+        bounding_boxes.shape,
+        '\n bounding_boxes:',
+        bounding_boxes)
     # print('points shape:', points.shape, '\n points:', points)
     print('找到人脸数目为：{}'.format(faces_num))
 
@@ -37,9 +47,20 @@ def read_photo(img):
 
     for i, face_position in enumerate(bounding_boxes):
         face_position = face_position.astype(int)
-        cv2.rectangle(frame, (face_position[0], face_position[1]), (face_position[2], face_position[3]), (0, 255, 0), 1)
-        cv2.circle(frame, (face_position[0], face_position[1]), 2, (0, 0, 255), -1)
-        cv2.circle(frame, (face_position[2], face_position[3]), 2, (0, 0, 255), -1)
+        cv2.rectangle(
+            frame,
+            (face_position[0],
+             face_position[1]),
+            (face_position[2],
+             face_position[3]),
+            (0,
+             255,
+             0),
+            1)
+        cv2.circle(
+            frame, (face_position[0], face_position[1]), 2, (0, 0, 255), -1)
+        cv2.circle(
+            frame, (face_position[2], face_position[3]), 2, (0, 0, 255), -1)
         w = face_position[2] - face_position[0]
         h = face_position[3] - face_position[1]
         S = w * h
@@ -67,7 +88,7 @@ def read_photo(img):
     cv2.putText(
         frame,
         'MAX',
-        (max_face_position[0], max_face_position[1]-15),
+        (max_face_position[0], max_face_position[1] - 15),
         cv2.FONT_HERSHEY_COMPLEX_SMALL,
         1,
         (255, 0, 0),
